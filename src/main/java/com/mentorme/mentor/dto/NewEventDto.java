a28ca968858a2a45c8c2e384bdb86717102e611f
andrei.sabou <andrei.sabou@3pillarglobal.com>
2018-05-23 15:23:02 +0300

    request body

---
+++ src/main/java/com/mentorme/mentor/dto/NewEventDto.java
@@ -0,0 +1,15 @@
+package com.mentorme.mentor.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class NewEventDto {
+
+   private Long categoryId;
+   private String description;
+   private Long userId;
+   private Long locationId;
+   private String name;
+}
