b35b94e326100411f645b26c2a87b95ad6efdb89
andrei.sabou <andrei.sabou@3pillarglobal.com>
2018-08-08 13:42:27 +0300

    CRUD - session

--- src/main/java/com/mentorme/mentor/MentorApplication.java
+++ src/main/java/com/mentorme/mentor/MentorApplication.java
@@ -8,11 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class MentorApplication {
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(MentorApplication.class, args);
 	}
